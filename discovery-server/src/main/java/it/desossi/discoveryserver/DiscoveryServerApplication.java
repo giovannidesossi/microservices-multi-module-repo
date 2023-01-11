9e17b21215b3f8fcec1a3e4e156173ef5c85a8f4
PC-CASA\giova <giovanni.desossi@gmail.com>
2023-01-11 16:41:43 +0100

    Lats modified

--- discovery-server/src/main/java/it/desossi/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/it/desossi/discoveryserver/DiscoveryServerApplication.java
@@ -1,8 +1,13 @@
 package it.desossi.discoveryserver;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DiscoveryServerApplication {
 
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+	
 }
