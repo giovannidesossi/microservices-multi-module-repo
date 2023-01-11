6ef19aa726a7db0354141435673179053e47cf29
PC-CASA\giova <giovanni.desossi@gmail.com>
2023-01-11 17:05:44 +0100

    Enabled Eureka clients

--- inventory-service/src/main/java/it/desossi/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/it/desossi/inventoryservice/InventoryServiceApplication.java
@@ -3,12 +3,14 @@ package it.desossi.inventoryservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import it.desossi.inventoryservice.model.Inventory;
 import it.desossi.inventoryservice.repository.InventoryRepository;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
