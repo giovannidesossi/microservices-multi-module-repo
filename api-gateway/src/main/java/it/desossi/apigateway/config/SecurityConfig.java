a519f51078d2bac89a2a3d0e375687f585bbb527
PC-CASA\giova <giovanni.desossi@gmail.com>
2023-01-20 18:09:27 +0100

    Update massivo

--- api-gateway/src/main/java/it/desossi/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/it/desossi/apigateway/config/SecurityConfig.java
@@ -12,14 +12,20 @@ public class SecurityConfig {
 	
 	@Bean
 	public SecurityWebFilterChain springSecurityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+		/**
+		 * When should you use CSRF protection? 
+		 * Our recommendation is to use CSRF protection for any request that could be processed by a browser by normal users. 
+		 * If you are creating a service that is used only by non-browser clients, you likely want to DISABLE CSRF protection.
+		 * (from https://docs.spring.io/spring-security/reference/features/exploits/csrf.html#csrf-when)
+		 */
 		serverHttpSecurity.csrf()
-			.disable()
+			.disable()//disabilitiamo csrf perchè usiamo Postman come client, cioè non faremo chiamate da browser
 			.authorizeExchange(exchange -> exchange
-						.pathMatchers("/eureka/**")
-						.permitAll()
-						.anyExchange()
-						.authenticated())
-			.oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+						.pathMatchers("/eureka/**")//path verso le risorse statiche di Eureka
+						.permitAll()//permettiamo tutte le chiamate alle risorse statiche di Eureka
+						.anyExchange()//ma ogni altra chiamata...
+						.authenticated())//...deve essere autenticata
+			.oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);//e abilitiamo le capacità di resource server (OAuth2), che saranno di tipo JWT
 		return serverHttpSecurity.build();
 	}
 
